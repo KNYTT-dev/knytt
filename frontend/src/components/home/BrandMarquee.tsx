a6877d8955cf68bc090168491efd958fde432dc9
Maxwell Huang <maxwellhuang@Maxwells-MBP.lan>
2025-12-15 15:09:25 -0800

    change file type

--- frontend/src/components/home/BrandMarquee.tsx
+++ frontend/src/components/home/BrandMarquee.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 // Single brand - Faherty
 const BRAND = {
   name: 'Faherty',
-  logo: '/logos/brands/faherty.svg', // Change to .png if using PNG
+  logo: '/logos/brands/faherty.jpg',
 };
 
 export function BrandMarquee() {
